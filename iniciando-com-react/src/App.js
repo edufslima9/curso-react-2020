6b7035d230a5f79a3793306bd876d4c10cc5ea53
Eduardo Felipe <edufslima9@gmail.com>
2020-11-26 11:59:37 -0300

    Adicionando comportamento ao componente para manipular o estado

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -6,9 +6,16 @@ class App extends Component {
     nome : 'Eduardo Felipe'
   }
 
+  modificarNome = (event) => {
+    this.setState({ nome : event.target.value });
+  }
+
   render() {
     return (
-      <h1>Hello, {this.state.nome}</h1>
+      <React.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello, {this.state.nome}</h1>
+      </React.Fragment>
     )
   }
 }
