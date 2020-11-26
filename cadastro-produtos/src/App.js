104c8d8f915165a9b2a4321ece59b5b1ad745b61
Eduardo Felipe <edufslima9@gmail.com>
2020-11-26 13:45:51 -0300

    Implementando o NavBar

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
+import NavBar from './components/navbar'
+
 function App() {
   return (
-    <div className="App">
-      <div className="alert alert-dismissible alert-warning">
-        <button type="button" className="close" data-dismiss="alert">&times;</button>
-        <h4 className="alert-heading">Warning!</h4>
-        <p className="mb-0">Test Warning, <a href="#" className="alert-link">Bootswatch</a>.</p>
-      </div>
-    </div>
+    <>
+      <NavBar/>
+    </>
   );
 }
 
